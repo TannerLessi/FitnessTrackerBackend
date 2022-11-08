0e354dc4d3bd2d1ab4634ad431b8599c5b41ff5e
tanner <tannerlessi@gmail.com>
2022-11-08 15:20:48 -0800

    ra routes not done

--- api/routine_activities.js
+++ api/routine_activities.js
@@ -1,21 +1,19 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { getRoutineById } = require("../db/adapters/routines");
 const routineActivitiesRouter = require("express").Router();
-const { addActivityToRoutine } = require("../db/adapters/routine_activities");
-const { JWT_SECRET, COOKIE_SECRET } = process.env;
+const {
+  addActivityToRoutine,
+  updateRoutineActivity,
+  getRoutineActivityById,
+  destroyRoutineActivity,
+} = require("../db/adapters/routine_activities");
+const { JWT_SECRET } = process.env;
 const { authRequired } = require("./ultis");
 
-routineActivitiesRouter.post("/routine_activities", async (req, res, next) => {
-  const { routine_id, activity_id, count, duration } = req.body;
-  const routineActivitiesData = {
-    routine_id,
-    activity_id,
-    count,
-    duration,
-  };
-
+routineActivitiesRouter.post("/", async (req, res, next) => {
   try {
-    const routineActivities = await addActivityToRoutine(routineActivitiesData);
+    const routineActivities = await addActivityToRoutine(req.body);
     console.log(routineActivities);
     if (routineActivities) {
       res.send({
@@ -29,3 +27,71 @@ routineActivitiesRouter.post("/routine_activities", async (req, res, next) => {
     next({ message });
   }
 });
+
+routineActivitiesRouter.patch(
+  "/:routineActivityId",
+  authRequired,
+  async (req, res, next) => {
+    const { routineActivityId } = req.params;
+    const { count, duration } = req.body;
+    const updateFields = {};
+
+    if (count) {
+      updateFields.count = count;
+    }
+    if (duration) {
+      updateFields.duration = duration;
+    }
+
+    try {
+      const originalRoutine = await getRoutineById(routineActivityId);
+      if (originalRoutine.creator_id === req.user.id) {
+        const updatedRoutineActivity = await updateRoutineActivity(
+          routineActivityId,
+          updateFields
+        );
+        res.send({ routine: updatedRoutineActivity });
+      } else {
+        next({
+          name: "UnauthorizedUserError",
+          message: "You cannot update a routine_activity that is not yours",
+        });
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  }
+);
+
+routineActivitiesRouter.delete(
+  "/:routineActivityId",
+  authRequired,
+  async (req, res, next) => {
+    try {
+      const { routineActivityId } = req.params;
+      const routineActivity = await getRoutineActivityById(routineActivityId);
+      const routine = await getRoutineById(routineActivity.routine_id);
+      if (routine.creator_id === req.user.id) {
+        destroyRoutineActivity(routineActivityId);
+        res.send({ Deleted: routineActivity });
+      } else {
+        next(
+          routineActivity
+            ? {
+                name: "Unauthorized User Error",
+                message:
+                  "You cannot delete a routine_activity which is not yours",
+              }
+            : {
+                name: "Routine_activity Not Found Error",
+                message: "That routine_activity does not exist",
+              }
+        );
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  }
+);
+
+module.exports = routineActivitiesRouter;
