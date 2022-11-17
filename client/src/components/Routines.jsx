6fd610eb41fbeb84075dc8947efa22dc55b2fe16
Cynthia <cluk@dynamicsignal.com>
2022-11-16 21:46:21 -0800

    Added activities to routines homepage

--- client/src/components/Routines.jsx
+++ client/src/components/Routines.jsx
@@ -9,6 +9,7 @@ import { useNavigate } from "react-router-dom";
 function RoutinesComponent() {
   const { routines, setRoutines } = useRoutines();
   const navigate = useNavigate();
+
   useEffect(() => {
     const getRoutines = async () => {
       const data = await fetchRoutines();
@@ -18,15 +19,27 @@ function RoutinesComponent() {
     getRoutines();
   }, []);
   console.log("routine", routines);
+
   return (
     <div>
       {routines.map((routine) => {
         return (
           <div>
             <div>
-              <h3> Creator:{routine.creatorName}</h3>
-              <div>name:{routine.name}</div>
-              <div>goal:{routine.goal}</div>
+              <h3>Creator:{routine.creatorName}</h3>
+              <p>Routine Name:{routine.name}</p>
+
+              {routine?.activities?.map((activity) => {
+                return (
+                  <div>
+                    <p>Activity Name: {activity.name}</p>
+                    <p>Description: {activity.description}</p>
+                    <p>Count: {activity.count}</p>
+                    <p>Duration: {activity.duration}</p>
+                  </div>
+                );
+              })}
+
               <button
                 onClick={() => {
                   navigate(`/routines/${routine.id}`);
